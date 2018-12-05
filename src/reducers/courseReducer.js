34138b41529ad65f4241ebd50b97dccdf80c9a21
Varun <varun241095@hotmail.com>
2018-12-05 17:36:17 +0530

    End of Module 11

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,22 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+
+    case types.CREATE_COURSE_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.courses)
+      ];
+
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.courses.id),
+        Object.assign({}, action.courses)
+      ];
 
     default:
       return state;
