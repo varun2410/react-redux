7c70527766b22bfa3a6f6fdc91953288295a147f
Varun <varun241095@hotmail.com>
2018-12-13 18:27:18 +0530

    End of Module 12

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,6 @@
 import AuthorApi from '../api/mockAuthorApi';
 import * as types from './actionTypes';
+import {beginAjaxCall} from "./ajaxStatusAction";
 
 export function loadAuthorSuccess(authors) {
   return { type: types.LOAD_AUTHOR_SUCCESS, authors };
@@ -7,6 +8,7 @@ export function loadAuthorSuccess(authors) {
 
 export function loadAuthors() {
   return dispatch => {
+    dispatch(beginAjaxCall());
     return AuthorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorSuccess(authors));
     }).catch(error => {
